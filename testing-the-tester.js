4e66d903c5f10694aeff6276115fea6285101c75
Lou Lee <louisa.leeyh@gmail.com>
2017-03-18 15:25:09 +0800

    finish

--- testing-the-tester.js
+++ testing-the-tester.js
@@ -9,15 +9,41 @@ function isGameOver() {
 }
 
 function whoWon() {
-  return 0
+  if ( //player 1 wins
+    (gameBoard[0] === 0 && gameBoard[2] === 1 && gameBoard[4] === 2) ||
+    (gameBoard[0] === 3 && gameBoard[2] === 4 && gameBoard[4] === 5) ||
+    (gameBoard[0] === 6 && gameBoard[2] === 7 && gameBoard[4] === 8) ||
+    (gameBoard[0] === 0 && gameBoard[2] === 3 && gameBoard[4] === 6) ||
+    (gameBoard[0] === 1 && gameBoard[2] === 4 && gameBoard[4] === 7) ||
+    (gameBoard[0] === 2 && gameBoard[2] === 5 && gameBoard[4] === 8) ||
+    (gameBoard[0] === 0 && gameBoard[2] === 4 && gameBoard[4] === 8) ||
+    (gameBoard[0] === 2 && gameBoard[2] === 4 && gameBoard[4] === 6)
+  ) {return 1}
+  else if ( //player 2 wins
+    (gameBoard[1] === 0 && gameBoard[3] === 1 && gameBoard[5] === 2) ||
+    (gameBoard[1] === 3 && gameBoard[3] === 4 && gameBoard[5] === 5) ||
+    (gameBoard[1] === 6 && gameBoard[3] === 7 && gameBoard[5] === 8) ||
+    (gameBoard[1] === 0 && gameBoard[3] === 3 && gameBoard[5] === 6) ||
+    (gameBoard[1] === 1 && gameBoard[3] === 4 && gameBoard[5] === 7) ||
+    (gameBoard[1] === 2 && gameBoard[3] === 5 && gameBoard[5] === 8) ||
+    (gameBoard[1] === 0 && gameBoard[3] === 4 && gameBoard[5] === 8) ||
+    (gameBoard[1] === 2 && gameBoard[3] === 4 && gameBoard[5] === 6)
+  ) {return 2}
+  else if (gameBoard.length >= 9) {return 3}
+  else {return 0}
 }
 
-function playTurn() {
-
+function playTurn(index) {
+  if (isGameOver() || gameBoard.includes(index)) {
+    return false
+  } else {
+    gameBoard.push(index)
+    console.log(gameBoard)
+    return true
+  }
 }
 
 function restart() {
   gameBoard = []
   player = 1
-
 }
